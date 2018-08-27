617cfb14a6fc0c3a562a602750a0b730b7ede73d
Scoti Dodson <scotidodson@gmail.com>
2018-08-27 20:43:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1 
     array.join
+  elsif array.length == 2 
+    array.join(" and ")
   else
     array << "and #{array.last}"
     array.delete_at(-2)
